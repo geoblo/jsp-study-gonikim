a2f1712c23b838ab688450740c85be2414d2f3c1
geoblo <geoblo.naver.com>
2025-10-27 18:25:12 +0900

    up

--- BookMarket/src/main/java/controller/ProcessAddBookServlet.java
+++ BookMarket/src/main/java/controller/ProcessAddBookServlet.java
@@ -29,7 +29,7 @@ import dto.Book;
 )
 public class ProcessAddBookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 
