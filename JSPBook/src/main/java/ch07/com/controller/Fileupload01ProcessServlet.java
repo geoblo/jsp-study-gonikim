9366ce427b57623b506067be2437f85047c8e6a6
geoblo <geoblo@naver.com>
2025-12-31 11:54:32 +0900

    up

--- JSPBook/src/main/java/ch07/com/controller/Fileupload01ProcessServlet.java
+++ JSPBook/src/main/java/ch07/com/controller/Fileupload01ProcessServlet.java
@@ -16,10 +16,10 @@ import jakarta.servlet.http.Part;
 // 서블릿 3.0에서 도입된 파일 업로드(멀티파트 요청) 처리를 위한 어노테이션
 // 이걸 선언하면 multipart/form-data 형식의 요청을 정상적으로 파싱 가능
 @MultipartConfig(
-//	location = "D:/upload", // 최종 저장될 업로드 경로
+	location = "D:/temp",                // 서버 내부 임시 폴더를 대체하는 사용자 정의 임시 폴더
 	fileSizeThreshold = 1024 * 1024 * 1, // 메모리 임시 저장 임계값(1MB) -> 파일을 메모리에 임시 저장할 최대 크기
-	maxFileSize = 1024 * 1024 * 10, // 업로드 최대 파일 크기(10MB) -> 업로드되는 개별 파일의 최대 크기
-	maxRequestSize = 1024 * 1024 * 50 // 전체 요청 크기(50MB) -> 요청 전체(모든 파일 + 모든 일반 폼 필드)의 총합 크기
+	maxFileSize = 1024 * 1024 * 10,      // 업로드 최대 파일 크기(10MB) -> 업로드되는 개별 파일의 최대 크기
+	maxRequestSize = 1024 * 1024 * 50    // 전체 요청 크기(50MB) -> 요청 전체(모든 파일 + 모든 일반 폼 필드)의 총합 크기
 )
 public class Fileupload01ProcessServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
