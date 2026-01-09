95efa3177b34296e7ed9f5109ef391e3b32ef2d7
geoblo <geoblo@naver.com>
2026-01-09 16:15:44 +0900

    up

--- BookMarket/src/main/java/mvc/controller/BoardController.java
+++ BookMarket/src/main/java/mvc/controller/BoardController.java
@@ -1,11 +1,15 @@
 package mvc.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import mvc.model.BoardDAO;
+import mvc.model.BoardDTO;
 
 // 게시판 컨트롤러
 @WebServlet("*.do") // 예제용
@@ -37,7 +41,28 @@ public class BoardController extends HttpServlet {
 	// Service 계층: 비즈니스 로직 부분
 	// 등록된 글 목록 가져오기
 	private void requestBoardList(HttpServletRequest request) {
+		BoardDAO dao = BoardDAO.getInstance();
 		
+		int pageNum = 1; // 현재 페이지 번호
+		if (!request.getParameter("pageNum").isEmpty()) {
+			pageNum = Integer.parseInt(request.getParameter("pageNum"));
+		}
+		
+		int limit = LIST_COUNT; // 한 번에 표시될 게시글 수: 5개
+		
+		String items = request.getParameter("items"); // 검색 조건(제목, 본문, 글쓴이)
+		String text = request.getParameter("text"); // 검색어
+		
+		int total_record = dao.getListCount(items, text); // 전체 레코드 수
+		ArrayList<BoardDTO> boardList = dao.getBoardList(pageNum, limit, items, text); // 게시글 목록
+		
+		// 총 페이지 수: 11.0 / 5.0 = 2.2 -> 3.0 -> 3페이지
+		int total_page = (int) Math.ceil((double) total_record / limit);
+		
+		request.setAttribute("pageNum", pageNum);
+		request.setAttribute("total_page", total_page);
+		request.setAttribute("total_record", total_record);
+		request.setAttribute("boardList", boardList);
 	}
 	
 	
